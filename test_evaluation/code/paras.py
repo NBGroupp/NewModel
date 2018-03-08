4b0992608d4a4f82e049572960405f316d911aa2
VegaLearning <805491725@qq.com>
2018-03-08 21:40:56 +0800

    Create paras.py

---
+++ test_evaluation/code/paras.py
@@ -0,0 +1,38 @@
+import math
+
+VOCAB_SIZE = 10000 #词典规模
+
+LEARNING_RATE = 0.01 #学习率
+LEARNING_RATE_DECAY_FACTOR =  1 #控制学习率下降的参数
+# MAX_GRAD_NORM = 5 #用于控制梯度膨胀的参数
+
+HIDDEN_SIZE = 128 #词向量维度
+PRE_CONTEXT_HIDDEN_SIZE = HIDDEN_SIZE #上文lstm的隐藏层数目
+PRE_CONTEXT_NUM_LAYERS = 1 #上文lstm的深度
+FOL_CONTEXT_HIDDEN_SIZE = HIDDEN_SIZE #下文lstm的隐藏层数目
+FOL_CONTEXT_NUM_LAYERS= 1 #下文lstm的深度
+
+
+VALID_BATCH_SIZE = TEST_BATCH_SIZE = 1 #测试数据batch的大小
+
+DATA_SIZE = 7132608
+TEST_DATA_SIZE = DATA_SIZE
+
+TEST_STEP_SIZE=math.ceil(TEST_DATA_SIZE / TEST_BATCH_SIZE)
+
+#文件路径
+DATA1_PATH = '../model_data/data1.7132608'
+DATA2_PATH = '../model_data/data2.7132608'
+DATA3_PATH = '../model_data/data3.7132608'
+TARGET_PATH = '../model_data/target.7132608'
+VOCAB_PATH = '../model_data/vocab.10000'
+
+CKPT_PATH = '../ckpt/'
+MODEL_NAME = 'model.ckpt'
+RESULT_PATH = '../results/results.txt'
+COST_PATH = '../logs/cost&accuracy_logs'
+TEST_RESULT_PATH = '../results/test_results.txt'
+
+stepinter = 1000 # 输出步频
+
+TP = FP = TN = FN = P = N = 0
