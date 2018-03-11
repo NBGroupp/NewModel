345e75de5d97c8d8a2d21aee1806bdb095f9b930
VegaLearning <805491725@qq.com>
2018-03-11 18:47:52 +0800

    Update paras.py

--- Candidate_Attentive_Proofreade-CAP/code/paras.py
+++ Candidate_Attentive_Proofreade-CAP/code/paras.py
@@ -1,35 +1,26 @@
 import math
 
 VOCAB_SIZE = 10000 #词典规模
-MAX_TEXT_LENGTH = 50 #最长文本长度
-
 LEARNING_RATE = 0.01 #学习率
 LEARNING_RATE_DECAY_FACTOR =  1 #控制学习率下降的参数
-KEEP_PROB = 0.2 #节点不Dropout的概率
-# MAX_GRAD_NORM = 5 #用于控制梯度膨胀的参数
-
-HIDDEN_SIZE = 512 #词向量维度
+KEEP_PROB = 0.6 #节点不Dropout的概率
+HIDDEN_SIZE = 128 #词向量维度
 PRE_CONTEXT_HIDDEN_SIZE = HIDDEN_SIZE #上文lstm的隐藏层数目
 PRE_CONTEXT_NUM_LAYERS = 1 #上文lstm的深度
 FOL_CONTEXT_HIDDEN_SIZE = HIDDEN_SIZE #下文lstm的隐藏层数目
 FOL_CONTEXT_NUM_LAYERS= 1 #下文lstm的深度
-
-
 VALID_BATCH_SIZE = TEST_BATCH_SIZE = 64 #测试数据batch的大小
-#VALID_NUM_STEP = TEST_NUM_STEP = 1 #测试数据截断长度
 TRAIN_BATCH_SIZE = 128 #训练数据batch的大小
-#TRAIN_NUM_STEP =35 #训练数据截断长度
-GRAM = 3 #LSTM的时间维度
 
 DATA_SIZE = 7132608
 TRAIN_DATA_SIZE = int(DATA_SIZE * 0.6)
 VALID_DATA_SIZE = int(DATA_SIZE * 0.2)
 TEST_DATA_SIZE = int(DATA_SIZE-VALID_DATA_SIZE-TRAIN_DATA_SIZE)
-
 VALID_STEP_SIZE=math.ceil(VALID_DATA_SIZE / VALID_BATCH_SIZE)
 TEST_STEP_SIZE=math.ceil(TEST_DATA_SIZE / TEST_BATCH_SIZE)
 #TRAIN_STEP_SIZE=math.ceil(TRAIN_DATA_SIZE / TRAIN_BATCH_SIZE)
 TRAIN_STEP_SIZE = 10000
+STEP_PRINT = 1000
 NUM_EPOCH = 10 # 迭代次数
 
 #文件路径
@@ -44,5 +35,3 @@ MODEL_NAME = 'model.ckpt'
 RESULT_PATH = '../results/results.txt'
 COST_PATH = '../logs/cost&accuracy_logs'
 TEST_RESULT_PATH = '../results/test_results.txt'
-
-stepinter = 1000
