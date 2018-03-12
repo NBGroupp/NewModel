10eb376d1334fa0ff03bcb6ad2ceab64f183a36f
VegaLearning <805491725@qq.com>
2018-03-12 12:35:25 +0800

    Update model.py

--- test_evaluation/code/model.py
+++ test_evaluation/code/model.py
@@ -32,8 +32,7 @@ class Proofreading_Model(object):
         # 定义预期输出，它的维度和上面维度相同
         self.targets = tf.placeholder(tf.int32, [batch_size,])
         embedding = tf.get_variable("embedding", [VOCAB_SIZE, HIDDEN_SIZE])  # embedding矩阵
-        #self.embedding = embedding
-        # pre_context_model
+       
         with tf.variable_scope('Pre') as scope:
             pre_cell = tf.contrib.rnn.BasicLSTMCell(num_units=PRE_CONTEXT_HIDDEN_SIZE, forget_bias=0.0,
                                                 state_is_tuple=True)
@@ -70,53 +69,26 @@ class Proofreading_Model(object):
             fol_outputs = fol_states
             self.fol_final_state = fol_states  #下文lstm的最终状态
 
-        # 综合两个lstm的数据，加权平均
-        # self.output = tf.add(pre_outputs,fol_outputs)/2
-
         # 简单拼接
         concat_output = tf.concat([pre_outputs[0][0], fol_outputs[0][0]],axis=-1)
-        #print(concat_output)
-        #self.concat_output = concat_output
+
         # 双线性attention
         with tf.variable_scope('bilinear'):  # Bilinear Layer (Attention Step)
             candidate_words_input_vector = tf.nn.embedding_lookup(embedding, self.candidate_words_input)
             bilinear_weight = tf.get_variable("bilinear_weight", [2 * HIDDEN_SIZE, HIDDEN_SIZE])  # embedding矩阵
             '''计算候选词与上下文的匹配度'''
             M = candidate_words_input_vector * tf.expand_dims(tf.matmul(concat_output, bilinear_weight), axis=1)  # M = [batch_size,candi_num,hidden_size]
-            #self.M = M
-            # attention概率(匹配度)
             alpha = tf.nn.softmax(tf.reduce_sum(M, axis=2))  # [batch_size,candi_num]
-            #self.alpha = alpha
-            # sum attention 模型
             bilinear_output = tf.reduce_sum(candidate_words_input_vector * tf.expand_dims(alpha, axis=2), axis=1)  # [batch, h]
 
-        # 全连接层
-        # weight = tf.get_variable("weight", [HIDDEN_SIZE, VOCAB_SIZE])
-        # bias = tf.get_variable("bias", [VOCAB_SIZE])
-        # self.logits = tf.matmul(output, weight) + bias
-
         # sotfmax层
-        #self.bilinear_output = bilinear_output
         softmax_prob = tf.layers.dense(bilinear_output, units=VOCAB_SIZE, activation=tf.nn.softmax,
                                          kernel_initializer=tf.random_uniform_initializer(-0.01,0.01))  # [batch_size, vocab_size#]
-
-        #self.softmax_prob = softmax_prob
         # 非候选词概率置0
         tmp_prob = softmax_prob * self.candidate_in_vocab
-        #self.tmp_prob = tmp_prob
         # 重算概率
         self.logits = tmp_prob / tf.expand_dims(tf.reduce_sum(tmp_prob, axis=1),axis=1)
         self.logits = tf.clip_by_value(self.logits, 1e-7, 1.0 - 1e-7)
-
-        ''' 定义交叉熵损失函数和平均损失。
-        logits中在vocab_size个结果中选择概率最大的结果与相应的targets结果比较计算loss值
-         返回一个 [batch_size] 的1维张量 '''
-        # loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example(
-        #    [self.logits], [self.targets],
-        #    [tf.ones([batch_size], dtype=tf.float32)])
-
-        # softmax + 交叉熵损失函数
-        #loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.targets)
 
         # 求交叉熵
         one_hot_labels = tf.one_hot(self.targets,VOCAB_SIZE)
@@ -127,10 +99,9 @@ class Proofreading_Model(object):
             self.ave_cost = tf.Variable(0.0, trainable=False, dtype=tf.float32)
             self.ave_cost_op = self.ave_cost.assign(tf.divide(
                 tf.add(tf.multiply(self.ave_cost, self.global_step), self.cost), self.global_step+1))
-            #global_step从0开始
+            # global_step从0开始
             tf.summary.scalar('cost', self.cost)
             tf.summary.scalar('ave_cost', self.ave_cost)
-        # 只在训练模型时定义反向传播操作。
 
         # 记录accuracy
         with tf.variable_scope('accuracy'):
@@ -142,23 +113,13 @@ class Proofreading_Model(object):
             # global_step从0开始
             tf.summary.scalar('accuracy', self.accuracy)
             tf.summary.scalar('ave_accuracy', self.ave_accuracy)
-            # 只在训练模型时定义反向传播操作。
-        # 只在训练模型时定义反向传播操作。
+
         if not is_training: return
-
-        # trainable_variables = tf.trainable_variables()
-        # trainable_variables = tf.all_variables()
-        # 控制梯度大小，定义优化方法和训练步骤。
-        # grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, trainable_variables), MAX_GRAD_NORM)
-        # optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
-        # self.train_op = optimizer.apply_gradients(zip(grads, trainable_variables))
-
         #self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate)
         self.train_op = optimizer.minimize(self.cost)
 
         self.merged_summary_op = tf.summary.merge_all() # 收集节点
-        #self.merged_summary_op = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES,scope))
         
 # 使用给定的模型model在数据data上运行train_op并返回在全部数据上的cost值
 def run_epoch(session, model, data, train_op, is_training, batch_size, step_size, char_set, file,
@@ -200,7 +161,7 @@ def run_epoch(session, model, data, train_op, is_training, batch_size, step_size
             cnt = max_cnt - batch_size
         x1 = dataX1[cnt:cnt + batch_size]  #  取前文
         x1,x1_seqlen = Pad_Zero(x1)# 补0
-        # print(x1)
+
         x2 = dataX2[cnt:cnt + batch_size]  #  取后文
         x2, x2_seqlen = Pad_Zero(x2)# 补0
 
@@ -209,8 +170,6 @@ def run_epoch(session, model, data, train_op, is_training, batch_size, step_size
         x3, _ = Pad_Zero(x3)
 
         y = dataY[cnt:cnt + batch_size]  #  取结果
-        # print(y)
-        #lstm迭代计算
 
         cost, pre_state, fol_state, outputs, _, _, ave_cost_op, ave_accuracy_op\
         = session.run([model.cost, model.pre_final_state, model.fol_final_state,
@@ -240,20 +199,9 @@ def run_epoch(session, model, data, train_op, is_training, batch_size, step_size
         statistics_evaluation(classes, target_index)
 
         # 写入到文件以及输出到屏幕
-        if (((step+1) % stepinter == 0) or ( step == 0 )) and file:
-            '''
-            # 输出softmax概率值
-            print("logits sum: ",np.sum(outputs,1))
-            print("target prob: ")
-            for i in range(batch_size):
-                print(outputs[i][y[i]],end=' ') 
-            print("\noutput prob: ")
-            for i in range(batch_size):
-                print(outputs[i][classes[i]],end=' ')  
-            print("\nclasses: ",classes)
-            '''
+        if (((step+1) % STEP_PRINT == 0) or ( step == 0 )) and file:
             end = time.clock()
-            print("%.1f setp/s" % (stepinter/(end-start)))
+            print("%.1f setp/s" % (STEP_PRINT/(end-start)))
             start = time.clock()
             print("After %d steps, cost : %.3f" % (step, total_costs / (step + 1)))
             file.write("After %d steps, cost : %.3f" % (step, total_costs / (step + 1)) + '\n')
@@ -268,7 +216,6 @@ def run_epoch(session, model, data, train_op, is_training, batch_size, step_size
     
     #收集并将cost加入记录
     if(is_training):
-        # print ('ave_cost = %.5f' % (total_costs / (step_size + 1)))
         summary_str = session.run(summary_op, feed_dict={model.pre_input: x1, model.fol_input: x2,
                                                          model.candidate_words_input: x3,
                                                          model.candidate_in_vocab: x4,
@@ -295,17 +242,12 @@ def Pad_Zero(x):
         col_len=x_seqlen[i]
         for j in range(col_len,max_len):
             x[i].append(0)
-
-    # print(x)
-    # print(x_seqlen)
     return x,x_seqlen
 
 def is_candidate(x):
     is_candi = np.zeros([len(x),VOCAB_SIZE],dtype=np.float32)
     for i,index in enumerate(x):
         is_candi[i,index] = 1.0
-    #for i in range(len(x)):
-        #is_candi[i][0] = 0.0
     return is_candi
 
 
