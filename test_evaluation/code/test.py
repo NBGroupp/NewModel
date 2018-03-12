957d2d01e72d92a12dbe8e8d92f53ff93e4ed80b
VegaLearning <805491725@qq.com>
2018-03-12 12:39:48 +0800

    Update test.py

--- test_evaluation/code/test.py
+++ test_evaluation/code/test.py
@@ -9,7 +9,11 @@ from paras import *
 
 #定义主函数并执行
 def main():
-    #train_data = data_init()
+    with open(DATA0_PATH, 'r', encoding='utf-8') as f:
+        rows = f.read().strip().split('\n')
+        data0 = [] 
+        for one in rows:
+            data0.append(int(one))
     with open(DATA1_PATH, 'r', encoding='utf-8') as f:
         rows = f.read().strip().split('\n')
         data1 = [one.split() for one in rows]
@@ -30,7 +34,6 @@ def main():
                 one[index]=int(ele)
     with open(TARGET_PATH, 'r', encoding='utf-8') as f:
         rows = f.read().strip().split('\n')
-        #target = [one.split() for one in rows]
         target = [] 
         for one in rows:
             target.append(int(one))
@@ -38,23 +41,19 @@ def main():
         global char_set
         char_set = f.read().split('\n')
     
-
-    test_data=(data1,data2,data3,target)
+    test_data=(data0,data1,data2,data3,target)
     
     initializer = tf.random_uniform_initializer(-0.01, 0.01)
     with tf.variable_scope("Proofreading_model", reuse=None, initializer=initializer):
         test_model = Proofreading_Model(False, TEST_BATCH_SIZE)
 
     saver = tf.train.Saver()
-    # with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
     with tf.Session() as session:
         ckpt = tf.train.get_checkpoint_state(CKPT_PATH)
         # 训练模型。
-        # if(os.path.exists("../ckpt/checkpoint")):
         if ckpt and ckpt.model_checkpoint_path:
             # 读取模型
             print("loading model...")
-            # saver.restore(session, "../ckpt/model.ckpt")
             saver.restore(session, ckpt.model_checkpoint_path)
             i = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])+1
             test_model.global_epoch=i
@@ -65,8 +64,6 @@ def main():
         # 测试模型。
         file = open(TEST_RESULT_PATH, 'w')
         print("In testing with model of epoch %d: " % (i-1))
-        #run_epoch(session, test_model, test_data, tf.no_op(), False,
-                 # TEST_BATCH_SIZE, TEST_BATCH_SIZE, char_set, False, False, False) 
         run_epoch(session, test_model, test_data, tf.no_op(), False,
                   TEST_BATCH_SIZE, TEST_STEP_SIZE, char_set, file,False,False)
         file.close()
